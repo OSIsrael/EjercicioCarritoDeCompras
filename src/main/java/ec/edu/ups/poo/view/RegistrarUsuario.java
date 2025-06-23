8b600e1d45b541edbecbff177a032d59f8f0717e
OSIsrael <israelos067@gmail.com>
2025-06-23 08:12:40 -0500

    FEAT(008): CLASE 20 DE JUNIO

---
+++ src/main/java/ec/edu/ups/poo/view/RegistrarUsuario.java
@@ -0,0 +1,11 @@
+package ec.edu.ups.poo.view;
+
+import javax.swing.*;
+
+public class RegistrarUsuario {
+    private JPanel panel1;
+    private JPanel panelPrincipal;
+    private JTextField txtUsuarioRe;
+    private JPasswordField txtContraRe;
+    private JButton btnRegistrarse;
+}
